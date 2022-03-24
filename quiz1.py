6c072a564cd4fc420bdc3259c2b6387b501cf897
Yechan Lee <pigyechan@gmail.com>
2022-03-24 22:31:23 +0900

    Quiz wk1

---
+++ quiz1.py
@@ -0,0 +1,23 @@
+# 1주차 과제
+
+1번
+
+```python
+firstName, lastName = input('이름과 성을 입력하시오: ').split()
+print("Hellow [",firstName,"][",lastName,"]")
+```
+
+2번
+
+```python
+price= int(input('얼마가 나왔나요?: '))
+people= int(input('몇 명이 드셨나요?: '))
+print(int(price/people), "원", sep='')
+```
+
+3번
+
+```python
+weight=int(input('몸무게를 입력하세요(단위는 kg): '))
+print('당신의 몸무게는 ',float(weight*2.204),'파운드 입니다.', sep='')
+```
