aac73288f0724ed4835cf290498bfa0a6c87cf88
Yechan Lee <pigyechan@gmail.com>
2022-05-18 23:52:45 +0900

    Delete GDSC(Algorithm Study)_wk1 Homework Part.2 directory

--- GDSC(Algorithm Study)_wk1 Homework Part.2/List_#10.py
+++
@@ -1,6 +0,0 @@
-nums = [1, 2, 3, 4, 5]
-sum = 0
-for i in nums:
-    sum += i
-
-print(sum/len(nums))
--- GDSC(Algorithm Study)_wk1 Homework Part.2/List_#8.py
+++
@@ -1,6 +0,0 @@
-nums = [1, 2, 3, 4, 5]
-sum = 0
-for i in nums:
-    sum = sum + i
-
-print(sum)
