55691ebff9fee5368a4a35a1d505161e591e1958
Yechan Lee <pigyechan@gmail.com>
2022-05-18 23:53:02 +0900

    Delete study wk2 directory

--- study wk2/#4.py
+++
@@ -1,6 +0,0 @@
-a = int(input())
-b = input().split(" ")
-c = list()
-for i in range(0, a):
-    c.append(int(b[i]))
-print(min(c), max(c))
--- study wk2/#5.py
+++
@@ -1,7 +0,0 @@
-a = list()
-for i in range(0,9):
-    a.append(int(input()))
-
-maxe = max(a)
-print(maxe)
-print(a.index(maxe)+1)
